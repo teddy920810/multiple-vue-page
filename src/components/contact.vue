71c53537bb82bef25b9da17b00ddf7049a7e3c32
teddy920810 <shiy0810@163.com>
2019-05-29 09:46:43 +0800

    'fullPage'

--- src/components/contact.vue
+++ src/components/contact.vue
@@ -1,54 +1,26 @@
 <template>
   <div>
-    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
-      <b-form-group
-        id="input-group-1"
-        label="Email address:"
-        label-cols-sm="4"
-        label-cols-lg="2"
-        label-for="input-1"
-        description="We'll never share your email with anyone else."
-      >
-        <b-form-input
-          id="input-1"
-          v-model="form.email"
-          type="email"
-          required
-          placeholder="Enter email"
-        ></b-form-input>
-      </b-form-group>
+    <full-page ref="fullpage" :options="options">
+      <!--<div class="section">
+        <button class="next" @click="$refs.fullpage.api.moveSectionDown()">Next</button>
+        Section 1
+      </div>
+      <div class="section">
+        <div class="slide" data-anchor="slide1">Slide 2.1</div>
+        <div class="slide" data-anchor="slide2">Slide 2.2</div>
+      </div>
+      <div class="section">
+        Section 3
+      </div>-->
 
-      <b-form-group id="input-group-2" label="Your Name:" label-cols-sm="4" label-cols-lg="2" label-for="input-2">
-        <b-form-input
-          id="input-2"
-          v-model="form.name"
-          required
-          placeholder="Enter name"
-        ></b-form-input>
-      </b-form-group>
-
-      <b-form-group id="input-group-3" label="Food:" label-cols-sm="4" label-cols-lg="2" label-for="input-3">
-        <b-form-select
-          id="input-3"
-          v-model="form.food"
-          :options="foods"
-          required
-        ></b-form-select>
-      </b-form-group>
-
-      <b-form-group id="input-group-4">
-        <b-form-checkbox-group v-model="form.checked" id="checkboxes-4">
-          <b-form-checkbox value="me">Check me out</b-form-checkbox>
-          <b-form-checkbox value="that">Check that out</b-form-checkbox>
-        </b-form-checkbox-group>
-      </b-form-group>
-
-      <b-button type="submit" variant="primary">Submit</b-button>
-      <b-button type="reset" variant="danger">Reset</b-button>
-    </b-form>
-    <b-card class="mt-3" header="Form Data Result">
-      <pre class="m-0">{{ form }}</pre>
-    </b-card>
+      <div class="section">Section 1</div>
+      <div class="section">
+        <div class="slide" data-anchor="slide1">Slide 2.1</div>
+        <div class="slide" data-anchor="slide2">Slide 2.2</div>
+      </div>
+      <div class="section">Section 3</div>
+      <div class="section">Section 4</div>
+    </full-page>
   </div>
 </template>
 
@@ -56,34 +28,25 @@
 export default {
   data () {
     return {
-      form: {
-        email: '',
-        name: '',
-        food: null,
-        checked: []
-      },
-      foods: [{ text: 'Select One', value: null }, 'Carrots', 'Beans', 'Tomatoes', 'Corn'],
-      show: true
+      options: {
+        // menu: '#menu',
+        // anchors: ['page1', 'page2', 'page3'],
+        sectionsColor: ['yellow', 'orange', '#C0C0C0', '#ADD8E6'],
+        afterLoad: this.afterLoad,
+        navigation: true
+      }
     }
   },
   methods: {
-    onSubmit (evt) {
-      evt.preventDefault()
-      alert(JSON.stringify(this.form))
-    },
-    onReset (evt) {
-      evt.preventDefault()
-      // Reset our form values
-      this.form.email = ''
-      this.form.name = ''
-      this.form.food = null
-      this.form.checked = []
-      // Trick to reset/clear native browser form validation state
-      this.show = false
-      this.$nextTick(() => {
-        this.show = true
-      })
+    afterLoad () {
+      console.log("Emitted 'after load' event.")
     }
   }
 }
 </script>
+<style scoped>
+.section {
+  text-align:center;
+  font-size: 3em;
+}
+</style>
--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
 import contact from './contact.vue'
-
+import 'fullpage.js/vendors/scrolloverflow' // Optional. When using scrollOverflow:true
+import VueFullPage from 'vue-fullpage.js'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import './../../style/fullpage.min.css'
 
+Vue.use(VueFullPage)
 Vue.use(BootstrapVue)
 /* eslint-disable no-new */
 new Vue({
